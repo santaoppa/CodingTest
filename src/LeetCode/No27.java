1981ab6e645992108d9a382789a68eefe4ed7984
santaoppa <ssanta1227@naver.com>
2023-05-30 08:41:07 +0900

    27. Remove Element

---
+++ src/LeetCode/No27.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class No27 {
+    public static void main(String args[]){
+        System.out.println(removeElement(new int[]{3,2,2,3}, 3));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != val)  nums[index++] = nums[i];
+        }
+
+        return index;
+    }
+}
