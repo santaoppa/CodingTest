354056c2634f0353e4b118c88f953ca9275266d3
santaoppa <ssanta1227@naver.com>
2023-09-16 11:59:31 +0900

    BFS

--- src/BOJ/No12851.java
+++ src/BOJ/No12851.java
@@ -1,4 +1,55 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class No12851 {
+
+    static int N, K, MAX=100000;
+    static boolean[] visited;
+    static Queue<Integer> queue;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[MAX+1];
+        queue = new LinkedList<Integer>();
+        queue.offer(N);
+
+        int time = 0;
+        while(true) {
+            int cnt = bfs();
+            if(cnt>0) {
+                System.out.println(time);
+                System.out.println(cnt);
+                break;
+            }
+            time++;
+        }
+    }//end of main
+
+    private static int bfs() {
+        int cnt = 0;
+        int size=queue.size();
+        for (int i = 0; i < size; i++) {
+            int cur = queue.poll();
+            visited[cur]=true;
+
+            if(cur==K) {
+                cnt++;//경우의수 카운트
+                continue;
+            }
+
+            if(cur*2 <= MAX && !visited[cur*2]) queue.offer(cur*2);
+            if(cur+1 <= MAX && !visited[cur+1]) queue.offer(cur+1);
+            if(cur-1 >= 0 && !visited[cur-1]) queue.offer(cur-1);
+        }
+        return cnt;
+    }
 }
